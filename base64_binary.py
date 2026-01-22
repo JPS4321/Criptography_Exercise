8b8c05afc5946907fe56ef0383c10133b5705ab4
JPS4321 <sol22102@uvg.edu.gt>
2026-01-22 17:59:14 -0600

    Investigacion Fase 1

--- base64_binary.py
+++ base64_binary.py
@@ -1,5 +1,6 @@
 import base64
-texto = "SG9sYQ==" ##Esto significa hola en base64. Lo encripte con uno online
+texto = "SG9sYQ==" #Esto significa hola en base64. Lo encripte con uno online
+#para convertir de base64 a binario se usa base64. No se si es legal
 bytes_decodificados = base64.b64decode(texto)
 binario = ''.join(format(byte, '08b') for byte in bytes_decodificados)
 print("La palabra en binario es:", binario)
